b679a8330f059441e97ec4c946631c8308d19bc3
amrita-20 <dubey.am@northeastern.edu>
2024-06-29 00:27:02 -0700

    added code for login and session api call from client

---
+++ src/services.js
@@ -0,0 +1,31 @@
+const { response } = require("express");
+
+export function getSession () {
+    return fetch("/api/v1/session", {
+        method: "GET"
+    })
+    .then(response => {
+        if(response.ok)
+            return response.json();
+        return response.json();
+    })
+    .catch( () => Promise.reject({ error: 'networkError' }) )
+}
+
+export function addSession (username) {
+    return fetch("/api/v/session", {
+        method: "POST",
+        headers: new Headers({
+            'content-type': 'application/json'
+          }),
+        body: JSON.stringify({ username }),
+    })
+    .then(response => {
+        if(response.ok)
+            return response.json();
+        return response.json();
+    })
+
+    .catch( () => Promise.reject({ error: 'networkError' }) )
+}
+
